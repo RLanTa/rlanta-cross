172a054570859f9e7d9a4598b6d9aecdc3c91068
RLanTa <55257038+RLanTa@users.noreply.github.com>
2021-03-15 22:15:17 +0300

    Güncelleme v0.0.1
    "__resource.lua" Yakında geçersiz olacağı için kaldırılıp, "fxmanifest.lua" eklendi.

---
+++ rlanta-cross/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'rlanta-cross' 
+
+author 'rlanta#8502'
+
+version '0.0.1'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'serverlua'
+}
